7cf98fac71e3316a10a1b9d2f487942e6a53e6b8
inaki <iiaranzadi@gmail.com>
2019-03-15 16:59:39 -0600

    Add db cleaning and style

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import pink from '@material-ui/core/colors/pink';
+import purple from '@material-ui/core/colors/purple';
 
 export default createMuiTheme({
     typography: {
         useNextVariants: true,
     },
     palette: {
-        primary: pink,
+        primary: purple,
     },
 });
